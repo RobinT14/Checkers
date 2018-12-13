cd5d3adc41e5cededa15fa6ae5eefa25e13ca5a9
Robin <ThePioneerGuy@Gmail.com>
2018-12-13 13:15:08 +0100

    piece

--- main.js
+++ main.js
@@ -44,8 +44,42 @@ window.onload = function(){
         //move the piece
         this.move = function(tile){
             //make the tile move
+            this.object.removeClass("selected");
+    if(!Board.isValidPlacetoMove(tile.place[0], tile.place[1])){
+        return false;
+    }
+
+    //make sure the tile doesn't make a move backwards
+    if(this.player == 1 && this.king == false){
+        if(tile.place[0] < this.place[0]){
+            return false;
         }
     }
+        else if(this.player == 2 && this.king == false){
+            if(tile.place[0] > this.place[0]){
+                return false;
+            }
+         }
+    
+    //remove the mark from Game.board
+    Game.board[this.place[0]][this.place[1]] = 0;
+    Game.board[tile.place[0]][tile.place[1]] = this.player;
+    this.place = [tile.place[0], tile.place[1]];
+
+    //change the css
+    this.object.css("top", Game[this.place[0]]);
+    this.object.css("left", Game[this.place[1]]);
+
+    //if the piece reaches the end on the opposide side; the piece will be a king
+    if(!this.king && (this.place[0] ==0 || this.position[0] == 7)){
+        this.makeKing();
+    }
+    Game.changePlayerTurn();
+    return true;
+    };
+
+    }
+}
 
     //calculate distance between places
     var distance = function (x1, x2, y1, y2){
