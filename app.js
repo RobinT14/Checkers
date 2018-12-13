8243fc59569f1dd6d170a64b1d4809d834dcacf3
Robin <ThePioneerGuy@Gmail.com>
2018-12-13 20:32:47 +0100

    without .html

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 
 var express = require("express");
 var http = require("http");
+var path = require("path");
+
+//var wss = require("./app/communication/webSocketServer");
 
 var port = process.argv[2];
 var app = express();
 
+app.set('views', path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
+
+
 app.use(express.static(__dirname + "/public"));
+
+var router = require("./routes/route");
+app.use("/", router);
+
 http.createServer(app).listen(port);
