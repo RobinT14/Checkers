e993520fbfa9293a116be5c3046f57d19d366176
Robin <ThePioneerGuy@Gmail.com>
2018-12-13 14:51:01 +0100

    t

--- todo.js
+++ todo.js
@@ -4,9 +4,39 @@ window.onload = function() {
   //Piece object - there are 24 instances of them in a checkers game
   function Piece (element, position) {
    
+    //moves the piece
+    this.move = function (tile) { 
+      this.element.removeClass('selected'); 
+      if(!Board.isValidPlacetoMove(tile.position[0], tile.position[1])) return false;
+      //make sure piece doesn't go backwards if it's not a king
+      if(this.player == 1 && this.king == false) {
+        if(tile.position[0] < this.position[0]) return false;
+      } else if (this.player == 2 && this.king == false) {
+        if(tile.position[0] > this.position[0]) return false;
+      }
+      //remove the mark from Board.board and put it in the new spot
+      Board.board[this.position[0]][this.position[1]] = 0;
+      Board.board[tile.position[0]][tile.position[1]] = this.player;
+      this.position = [tile.position[0], tile.position[1]];
+      //change the css using board's dictionary
+      this.element.css('top', Board.dictionary[this.position[0]]);
+      this.element.css('left', Board.dictionary[this.position[1]]);
+      //if piece reaches the end of the row on opposite side crown it a king (can move all directions)
+      if(!this.king && (this.position[0] == 0 || this.position[0] == 7 )) 
+        this.makeKing();
+      Board.changePlayerTurn();
+      return true;
+    };
     
-    
-    
+    //tests if piece can jump anywhere
+    this.canJumpAny = function () {
+      if(this.canOpponentJump([this.position[0]+2, this.position[1]+2]) ||
+         this.canOpponentJump([this.position[0]+2, this.position[1]-2]) ||
+         this.canOpponentJump([this.position[0]-2, this.position[1]+2]) ||
+         this.canOpponentJump([this.position[0]-2, this.position[1]-2])) {
+        return true;
+      } return false;
+    };
     
     //tests if an opponent jump can be made to a specific place
     this.canOpponentJump = function(newPosition) {
@@ -60,6 +90,89 @@ window.onload = function() {
     }
   }
   
+  function Tile (element, position) {
+    //linked DOM element
+    this.element = element;
+    //position in gameboard
+    this.position = position;
+    //if tile is in range from the piece
+    this.inRange = function(piece) {
+      if(dist(this.position[0], this.position[1], piece.position[0], piece.position[1]) == Math.sqrt(2)) {
+        //regular move
+        return 'regular';
+      } else if(dist(this.position[0], this.position[1], piece.position[0], piece.position[1]) == 2*Math.sqrt(2)) {
+        //jump move
+        return 'jump';
+      }
+    };
+  }
+  /* 
+  //Board object - controls logistics of game
+  var Board = {
+    board: gameBoard,
+    playerTurn: 1,
+    tilesElement: $('div.tiles'),
+    //dictionary to convert position in Board.board to the viewport units
+    dictionary: ["0vmin", "10vmin", "20vmin", "30vmin", "40vmin", "50vmin", "60vmin", "70vmin", "80vmin", "90vmin"],
+    //initialize the 8x8 board
+    initalize: function () {
+      var countPieces = 0;
+      var countTiles = 0;
+      for (row in this.board) { //row is the index
+        for (column in this.board[row]) { //column is the index
+          //whole set of if statements control where the tiles and pieces should be placed on the board
+          if(row%2 == 1) {
+            if(column%2 == 0) {
+              this.tilesElement.append("<div class='tile' id='tile"+countTiles+"' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
+              tiles[countTiles] = new Tile($("#tile"+countTiles), [parseInt(row), parseInt(column)]);
+              countTiles += 1;
+            }
+          } else {
+            if(column%2 == 1) {
+              this.tilesElement.append("<div class='tile' id='tile"+countTiles+"' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
+              tiles[countTiles] = new Tile($("#tile"+countTiles), [parseInt(row), parseInt(column)]);
+              countTiles += 1;
+            }
+          }
+          if(this.board[row][column] == 1) {
+            $('.player1pieces').append("<div class='piece' id='"+countPieces+"' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
+            pieces[countPieces] = new Piece($("#"+countPieces), [parseInt(row), parseInt(column)]);
+            countPieces += 1;
+          } else if(this.board[row][column] == 2) {
+            $('.player2pieces').append("<div class='piece' id='"+countPieces+"' style='top:"+this.dictionary[row]+";left:"+this.dictionary[column]+";'></div>");
+            pieces[countPieces] = new Piece($("#"+countPieces), [parseInt(row), parseInt(column)]);
+            countPieces += 1;
+          }
+        }
+      }
+    },
+    //check if the location has an object
+    isValidPlacetoMove: function (row, column) {
+      console.log(row); console.log(column); console.log(this.board);
+      if(this.board[row][column] == 0) {
+        return true;
+      } return false;
+    },
+    //change the active player - also changes div.turn's CSS
+    changePlayerTurn: function () {
+      if(this.playerTurn == 1) {
+        this.playerTurn = 2;
+        $('.turn').css("background", "linear-gradient(to right, transparent 50%, #BEEE62 50%)");
+        return;
+      }
+      if(this.playerTurn == 2) {
+        this.playerTurn = 1;
+        $('.turn').css("background", "linear-gradient(to right, #BEEE62 50%, transparent 50%)");
+      }
+    },
+    //reset the game
+    clear: function () {
+      location.reload(); 
+    }
+  }
+  
+  //initialize the board
+  Board.initalize();
   
   /***
   Events
