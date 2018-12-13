a81f3ba1373e1f6b4f4cc642bf0ce2f50519f7dc
Robin <ThePioneerGuy@Gmail.com>
2018-12-13 12:27:10 +0100

    1

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ window.onload = function(){
     var pieces = [];
     var tiles = [];
 
+    
+
     //function to set pieces to the board
     function piece (object, place){
         this.object = object;
@@ -28,6 +30,8 @@ window.onload = function(){
         else{
             this.player = "player2";
         }
+
+        
         
         
         //check whether it is a big piece or not
@@ -47,6 +51,23 @@ window.onload = function(){
     var distance = function (x1, x2, y1, y2){
         var calculate1 = Math.pow(x1-x2) + Math.pow(y1-y2);
         return Math.sqrt(calculate1);
+    }
+
+    //check if a move is possible
+    function Tile (object, place){
+    this.object = object; // linked DOm element
+    this.place = place; //place on the board
+    //look if tile is in range for  a possible move
+        this.inRange = function(piece){
+        //look if move is just 1 tile; Pythagoras 1;1;sqrt(2)
+        if(dispatchEvent(this.position[0], this.position[1], piece.position[0] piece.position[1] == Math.sqrt(2))){
+            return "regular"; // regular move
+        };
+        //look if move is over an enemy; Pythagoras 2;2;sqrt(8)
+        else if(dispatchEvent(this.position[0], this.position[1], piece.position[0], piece.position[1]) ==  Math.sqrt(8)){
+        return "Jump";
+            }
+        }
     }
 
     //initialize game
