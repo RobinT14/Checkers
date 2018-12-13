afc22535490b51f0ecc8947e660ef21586b8d275
Robin <ThePioneerGuy@Gmail.com>
2018-12-13 21:38:05 +0100

    br

--- public/home.js
+++ public/home.js
@@ -1,11 +1,24 @@
+window.onload = function(){
+    
+    //play button 1v1
+$("#play").on("click", function() {
+    res.sendFile("game.html", {root: "./public"});
+});
+
+//play battle royal button
+$("#Battle").on("click", function() {
+   alert("Battle Royal Mode hasn't been released yet");
+});
+
+}
 
 
 
 
 //play button 1v1
-$("#play").on("click", function() {
-    alert("Help!");
-});
+//$("#play").on("click", function() {
+  //  alert("Help!");
+//});
 
  //play online button
  //$("#br").on("click", function() {
