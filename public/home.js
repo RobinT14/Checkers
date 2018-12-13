1b74d9c1d068a2342965a7667f6cf517fbe80b0b
Robin <ThePioneerGuy@Gmail.com>
2018-12-13 21:14:08 +0100

    last

---
+++ public/home.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+
+
+
+//play button 1v1
+$("#play").on("click", function() {
+    Game.reset();
+});
+
+ //play online button
+ //$("#br").on("click", function() {
+   // res.sendFile("game.html", {root: "./public"});
+//});
+
+ /*/show statistics button
+ $("#statistics").on("click", function() {
+    Game.reset();
+});
+
+*/
+
+}
