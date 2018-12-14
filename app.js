e46cb9e0136d004fb09a12d0b1e9c2a46ec1a57e
Robin <ThePioneerGuy@Gmail.com>
2018-12-14 03:16:23 +0100

    t

--- app.js
+++ app.js
@@ -9,23 +9,30 @@ var path = require("path");
 var port = process.argv[2];
 var app = express();
 
-/*app.use("/", function(req, res) {
-    res.sendFile("index.js", {root: "./"});// add file
-});
-
-
 var server = http.createServer(app);
 
-//const wss = new websocket.Server({ server });
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({
+    port: 8080,
+});
 
 wss.on("connection", function(ws){
+    var user = new User;
+});
 
-    ws.on("message", function incomming(message){
-        console.log("[LOG] + message");
-    })
+function User(ws){
+    this.ws = ws;
+    console.log("A user connected");
+    var user = this;
+}
 
-})
-*/
+module.exports.broadcast = function(data){
+    if(wss == null){
+      return false;
+    }
+}
+
+
 //view engine
 app.set('views', path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
@@ -43,8 +50,28 @@ http.createServer(app).listen(port);
 
 
 
+
+
+/*const ws = new WebSocket("ws://localhost:3000", {
+    perMessageDeflate: false
+});
 //module websocket
 //var wss = require("webSocketServer")//add location ^^^
 
 //setup wss
 //wss.start();
+
+
+/*app.use("/", function(req, res) {
+    res.sendFile("index.js", {root: "./"});// add file
+});
+
+
+wss.on("connection", function(ws){
+
+    ws.on("message", function incomming(message){
+        console.log("[LOG] + message");
+    })
+
+})
+*/
